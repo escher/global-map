5f9e2fc9cc37ae54799a2802a217d7d8c21541b4
Zak King <zaking17@gmail.com>
2019-05-08 14:15:57 -0700

    working full screen

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ import EscherContainer from './EscherContainer'
 import map1 from './data/iJO1366.Central metabolism.json'
 import map2 from './data/iJO1366.Fatty acid beta-oxidation.json'
 import map3 from './data/iJO1366.Fatty acid biosynthesis (saturated).json'
-// import model from './data/iJO1366.json'
+import model from './data/iJO1366.json'
+
+import _ from 'underscore'
+
+const data = _.chain(model.reactions)
+      .map(reaction => [reaction.id, Math.random()])
+      .object()
+      .value()
 
 class App extends Component {
   render () {
@@ -14,7 +21,7 @@ class App extends Component {
       <div className='App'>
         {[map1, map2, map3].map(mapData => (
           <div style={{height: '100%', width: '33%', float: 'left'}}>
-            <EscherContainer mapData={mapData} />
+            <EscherContainer mapData={mapData} reactionData={data} />
           </div>
         ))}
       </div>
--- src/EscherContainer.js
+++ src/EscherContainer.js
@@ -3,20 +3,18 @@
 import { h, Component } from 'preact'
 import { Builder } from 'escher'
 
-class EscherContainer extends Component {
+class EscherContainerMap extends Component {
   constructor (props) {
     super(props)
     this.state = {
       builder: null
     }
-    this.fullScreen = this.fullScreen.bind(this)
   }
 
   fullScreen () {
     const builder = this.state.builder
-    if (builder) {
+    if (builder && !builder.isFullScreen) {
       builder.fullScreen()
-      builder.map.draw_everything()
     }
   }
 
@@ -32,6 +30,7 @@ class EscherContainer extends Component {
       null,
       this.base,
       {
+        reaction_data: this.props.reactionData,
         fill_screen: false,
         menu: 'none',
         enable_editing: false,
@@ -43,6 +42,18 @@ class EscherContainer extends Component {
           scroll_behavior: 'pan',
           menu: 'all',
           simplified: false
+        },
+        first_load_callback: builder => {
+          // no interactions
+          builder.none_mode()
+          builder.callback_manager.set('full_screen', isFullScreen => {
+            builder.map.draw_everything()
+            if (!isFullScreen) {
+              builder.none_mode()
+            } else {
+              builder.zoom_mode()
+            }
+          })
         }
       }
     )
@@ -54,10 +65,32 @@ class EscherContainer extends Component {
       <div
         className='global-maps-escher-container'
         style={{width: '100%', height: '100%'}}
-        onClick={this.fullScreen}
       />
     )
   }
 }
 
+class EscherContainer extends Component {
+  constructor (props) {
+    super(props)
+    this.fullScreen = this.fullScreen.bind(this)
+  }
+
+  fullScreen () {
+    this.ec.fullScreen()
+  }
+
+  render () {
+    return (
+      <div onClick={this.fullScreen}
+        style={{width: '100%', height: '100%'}}
+      >
+        <EscherContainerMap
+          ref={ref => { this.ec = ref }}
+          {...this.props}
+        />
+      </div>
+    )
+  }
+}
 export default EscherContainer
